fd8db5260d4763ed13058b2dd2680997310e928c
devjeel <jeelhp02@gmail.com>
2019-09-24 21:00:28 -0400

    Initial Commit

---
+++ routes/lab2.js
@@ -0,0 +1,44 @@
+﻿var express = require("express");
+var router = express.Router();
+
+//GET lab2
+router.get('/', function (req, res) {
+
+    //Error checks
+    if (!(req.query.method && req.query.x && req.query.y)) {
+        Console.log('please provide method, x and y queries');
+
+    } else {
+        //storing values 
+        var method = req.query.method;
+        var x = parseInt(req.query.x);
+        var y = parseInt(req.query.y);
+        var result = 0; 
+        var operator = "";
+
+        if (method == "add") {
+
+            result = x + y;
+            operator = "+";
+
+        } else if (method == "subtract") {
+
+            result = x - y;
+            operator = "-";
+
+        } else if (method == "multiply") {
+
+            result = x * y;
+            operator = "*";
+
+        } else if (method == "divide") {
+            result = x / y;
+            operator = "/";
+        }
+
+        res.send("Result output is = " + x + operator + y + " = " + result);
+    }
+    
+})
+
+module.exports = router;
